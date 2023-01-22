9e20fd1328b0b1832ab824b6bd9c508c384afb66
ltwes606 <ltwes606@gmail.com>
2023-01-22 18:28:16 +0900

    회원 엔티티 구현

---
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,16 @@
+package hello.core.member;
+
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+    @Test
+    void join() {
+        // given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        // when
+
+
+        // then
+    }
+}
