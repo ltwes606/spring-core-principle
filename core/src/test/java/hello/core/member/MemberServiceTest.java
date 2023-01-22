069a6bf49f7fd64117e1085ef09b71ee8e1b3749
ltwes606 <ltwes606@gmail.com>
2023-01-22 18:46:30 +0900

    메모리 회원 저장소 구현

--- core/src/test/java/hello/core/member/MemberServiceTest.java
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,16 +1,22 @@
 package hello.core.member;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
+
+    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+
     @Test
     void join() {
         // given
         Member member = new Member(1L, "memberA", Grade.VIP);
 
         // when
-
+        memberRepository.save(member);
 
         // then
+        assertThat(memberRepository.findById(1L)).isSameAs(member);
     }
 }
