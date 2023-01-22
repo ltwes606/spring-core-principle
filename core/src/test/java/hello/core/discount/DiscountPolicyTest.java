5c3b7b220bdb30af40baeeb646db3b8bd11e6e5e
ltwes606 <ltwes606@gmail.com>
2023-01-22 22:59:54 +0900

    할인 정책 구현

---
+++ core/src/test/java/hello/core/discount/DiscountPolicyTest.java
@@ -0,0 +1,52 @@
+package hello.core.discount;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class DiscountPolicyTest {
+
+    @Test
+    void VIPFixDiscount() {
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        DiscountPolicy discountPolicy = new FixDiscountPolicy();
+        int discountAmount = discountPolicy.discount(member, 5000);
+
+        assertThat(discountAmount).isEqualTo(1000);
+    }
+
+    @Test
+    void basicFIxDiscount() {
+        Member member = new Member(1L, "memberA", Grade.BASIC);
+
+        DiscountPolicy discountPolicy = new FixDiscountPolicy();
+        int discountAmount = discountPolicy.discount(member, 5000);
+
+        assertThat(discountAmount).isEqualTo(0);
+    }
+
+    @Test
+    void VIPRateDiscount() {
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        DiscountPolicy discountPolicy = new RateDiscountPolicy();
+        int discountAmount = discountPolicy.discount(member, 5000);
+
+        assertThat(discountAmount).isEqualTo(500);
+    }
+
+    @Test
+    void basicRateDiscount() {
+        Member member = new Member(1L, "memberA", Grade.BASIC);
+
+        DiscountPolicy discountPolicy = new RateDiscountPolicy();
+        int discountAmount = discountPolicy.discount(member, 5000);
+
+        assertThat(discountAmount).isEqualTo(0);
+    }
+}
