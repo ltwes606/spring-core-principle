b0a026164ada6418da22773ef214302005f4eb60
ltwes606 <ltwes606@gmail.com>
2023-01-27 03:15:36 +0900

    lombok 설치

--- core/src/main/java/hello/core/member/MemberService.java
+++ core/src/main/java/hello/core/member/MemberService.java
@@ -1,6 +1,5 @@
 package hello.core.member;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
--- core/src/test/java/hello/core/beanFind/ApplicationContextSameBeanFindTest.java
+++ core/src/test/java/hello/core/beanFind/ApplicationContextSameBeanFindTest.java
@@ -3,7 +3,6 @@ package hello.core.beanFind;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import hello.core.discount.DiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import java.util.Map;
